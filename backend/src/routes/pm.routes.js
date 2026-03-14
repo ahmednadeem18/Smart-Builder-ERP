85e47cfe6ed610ac2cbceff9931815ab5a765131
RanaAbdulMoeez <ranaabdulmoeez987@gmail.com>
2026-03-14 16:25:24 +0500

    Update pm.routes.js

--- backend/src/routes/pm.routes.js
+++ backend/src/routes/pm.routes.js
@@ -1,1 +1,22 @@
+import express from 'express';
+import * as controller from '../controllers/pm.controller.js';
 
+const router = express.Router();
+
+/* Fetches all projects assigned to the specific PM.
+ */
+router.get('/projects/:managerId', controller.GetMyProjects);
+
+/* Retrieves the history of updates for a project.
+ */
+router.get('/project/logs/:projectId', controller.GetProjectHistory);
+
+/*Add Log 
+*/
+router.post('/log', controller.AddProgressUpdate);
+
+/*requesting different services
+*/
+router.post('/request/:type', controller.SubmitResourceRequest);
+
+export default router;
